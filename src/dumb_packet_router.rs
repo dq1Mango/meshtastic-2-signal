cd025387f4edde50ed3fac65c73aff440ccd1edd
Thomas Ranney <thomas@ranney.com>
2025-12-30 13:48:35 -0500

    oh woah i actually made smthn today

--- src/dumb_packet_router.rs
+++ src/dumb_packet_router.rs
@@ -17,11 +17,13 @@ impl std::error::Error for MyError {}
 
 impl PacketRouter<String, MyError> for DumbPacketRouter {
   fn handle_packet_from_radio(&mut self, packet: meshtastic::protobufs::FromRadio) -> Result<String, MyError> {
+    println!("not rly handling packet from radio ngl");
     Ok("hi".to_string())
   }
 
   fn handle_mesh_packet(&mut self, packet: meshtastic::protobufs::MeshPacket) -> Result<String, MyError> {
-    Ok("".to_string())
+    println!("not rly handling packet ngl");
+    Ok("bruh".to_string())
   }
 
   fn source_node_id(&self) -> meshtastic::types::NodeId {
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use dumb_packet_router::DumbPacketRouter;
 use dumb_packet_router::MyError;
 
 use meshtastic::api::StreamApi;
-use meshtastic::packet::PacketRouter;
-use meshtastic::protobufs::ChannelSettings;
-use meshtastic::protobufs::config::DeviceConfig;
-use meshtastic::protobufs::config::NetworkConfig;
+use meshtastic::packet::PacketDestination;
+use meshtastic::protobufs::FromRadio;
+use meshtastic::types::EncodedMeshPacketData;
+use meshtastic::types::MeshChannel;
 use meshtastic::utils;
 
 // This import allows for decoding of mesh packets
@@ -21,6 +21,14 @@ use meshtastic::protobufs;
 use std::collections::HashMap;
 
 // use tokio::select;
+pub enum Action {
+  SendToMesh {
+    body: String,
+    channel: MeshChannel,
+    destination: PacketDestination,
+  },
+  FromRadio(FromRadio),
+}
 
 type Nodes = HashMap<usize, meshtastic::protobufs::NodeInfo>;
 
@@ -49,35 +57,73 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   let mut nodes = HashMap::<usize, meshtastic::protobufs::NodeInfo>::new();
 
-  let channel_config = protobufs::Channel {
-    index: 1,
-    settings: Some(ChannelSettings {
-      psk: vec![],
-      name: "gateway".to_string(),
-      id: 67,
-      uplink_enabled: true,
-      downlink_enabled: true,
-      module_settings: None,
-      channel_num: 0,
-    }),
-    role: 2,
-  };
+  // let channel_config = protobufs::Channel {
+  //   index: 1,
+  //   settings: Some(ChannelSettings {
+  //     psk: vec![],
+  //     name: "gateway".to_string(),
+  //     id: 67,
+  //     uplink_enabled: true,
+  //     downlink_enabled: true,
+  //     module_settings: None,
+  //     channel_num: 0,
+  //   }),
+  //   role: 2,
+  // };
 
   let mut packet_router = DumbPacketRouter;
-  println!(
-    "{:?}",
-    stream_api
-      .update_channel_config::<String, MyError, DumbPacketRouter>(&mut packet_router, channel_config)
-      .await
-  );
-  return Ok(());
+  // println!(
+  //   "{:?}",
+  //   stream_api
+  //     .update_channel_config::<String, MyError, DumbPacketRouter>(&mut packet_router, channel_config)
+  //     .await
+  // );
 
   // This loop can be broken with ctrl+c or by disconnecting
   // the attached serial port.
-  while let Some(decoded) = decoded_listener.recv().await {
-    // println!("Received: {:?}", decoded);
-    handle_from_radio_packet(decoded, &mut nodes);
+
+  loop {
+    let soon_to_be_legacy = decoded_listener.recv().await;
+
+    let mut current_action = if let Some(decdoed) = soon_to_be_legacy {
+      Some(Action::FromRadio(decdoed))
+    } else {
+      break;
+    };
+
+    while let Some(action) = current_action {
+      current_action = match action {
+        Action::FromRadio(decoded) => handle_from_radio_packet(decoded, &mut nodes),
+
+        Action::SendToMesh {
+          body,
+          channel,
+          destination,
+        } => {
+          println!("\tsending to mesh...");
+          println!(
+            "{:?}",
+            stream_api
+              .send_mesh_packet(
+                &mut packet_router,
+                body.into_bytes().into(),
+                protobufs::PortNum::TextMessageApp,
+                destination,
+                channel,
+                true,
+                false,
+                true,
+                None,
+                None,
+              )
+              .await
+          );
+          None
+        }
+      }
+    }
   }
+  // println!("Received: {:?}", decoded);
 
   // Note that in this specific example, this will only be called when
   // the radio is disconnected, as the above loop will never exit.
@@ -88,19 +134,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-fn print_node_info(info: protobufs::NodeInfo) {}
+// fn print_node_info(info: protobufs::NodeInfo) {}
 
 /// A helper function to handle packets coming directly from the radio connection.
 /// The Meshtastic `PhoneAPI` will return decoded `FromRadio` packets, which
 /// can then be handled based on their payload variant. Note that the payload
 /// variant can be `None`, in which case the packet should be ignored.
-fn handle_from_radio_packet(from_radio_packet: meshtastic::protobufs::FromRadio, nodes: &mut Nodes) {
+fn handle_from_radio_packet(
+  from_radio_packet: meshtastic::protobufs::FromRadio,
+  nodes: &mut Nodes,
+) -> Option<Action> {
+  // let cloned_packet = from_radio_packet.clone();
   // Remove `None` variants to get the payload variant
   let payload_variant = match from_radio_packet.payload_variant {
     Some(payload_variant) => payload_variant,
     None => {
       println!("Received FromRadio packet with no payload variant, not handling...");
-      return;
+      return None;
     }
   };
 
@@ -112,15 +162,17 @@ fn handle_from_radio_packet(from_radio_packet: meshtastic::protobufs::FromRadio,
       println!("Received channel packet: {:?}", channel);
     }
     meshtastic::protobufs::from_radio::PayloadVariant::NodeInfo(node_info) => {
-      println!("Received node info packet: {:?}", node_info);
+      // println!("Received node info packet: {:?}", node_info);
     }
     meshtastic::protobufs::from_radio::PayloadVariant::Packet(mesh_packet) => {
-      handle_mesh_packet(mesh_packet);
+      return handle_mesh_packet(mesh_packet);
     }
     _ => {
       println!("Received other FromRadio packet, not handling...");
     }
   };
+
+  None
 }
 
 /// A helper function to handle `MeshPacket` messages, which are a subset
@@ -130,22 +182,20 @@ fn handle_from_radio_packet(from_radio_packet: meshtastic::protobufs::FromRadio,
 ///
 /// Mesh packets are the most commonly used type of packet, and are usually
 /// what people are referring to when they talk about "packets."
-fn handle_mesh_packet(mesh_packet: meshtastic::protobufs::MeshPacket) {
+fn handle_mesh_packet(mesh_packet: protobufs::MeshPacket) -> Option<Action> {
+  let cloned_packet = mesh_packet.clone();
   // Remove `None` variants to get the payload variant
-  let payload_variant = match mesh_packet.payload_variant {
-    Some(payload_variant) => payload_variant,
-    None => {
-      println!("Received mesh packet with no payload variant, not handling...");
-      return;
-    }
-  };
 
   // Only handle decoded (unencrypted) mesh packets
-  let packet_data = match payload_variant {
-    meshtastic::protobufs::mesh_packet::PayloadVariant::Decoded(decoded_mesh_packet) => decoded_mesh_packet,
-    meshtastic::protobufs::mesh_packet::PayloadVariant::Encrypted(_encrypted_mesh_packet) => {
+  let packet_data = match mesh_packet.payload_variant {
+    Some(protobufs::mesh_packet::PayloadVariant::Decoded(decoded_mesh_packet)) => decoded_mesh_packet,
+    Some(protobufs::mesh_packet::PayloadVariant::Encrypted(_encrypted_mesh_packet)) => {
       println!("Received encrypted mesh packet, not handling...");
-      return;
+      return None;
+    }
+    None => {
+      println!("Received mesh packet with no payload variant, not handling...");
+      return None;
     }
   };
 
@@ -162,11 +212,39 @@ fn handle_mesh_packet(mesh_packet: meshtastic::protobufs::MeshPacket) {
 
       println!("Received position packet: {:?}", decoded_position);
     }
-    meshtastic::protobufs::PortNum::TextMessageApp => {
-      let decoded_text_message = String::from_utf8(packet_data.payload).unwrap();
 
-      println!("Received text message packet: {:?}", decoded_text_message);
-    }
+    meshtastic::protobufs::PortNum::TextMessageApp => match mesh_packet.channel {
+      0 => {
+        // println!("heres the whole packet: {:#?}", &cloned_packet);
+        let decoded_text_message = String::from_utf8(packet_data.payload).unwrap();
+
+        println!("Received DM message: {:?}", &decoded_text_message);
+
+        if decoded_text_message == "/ping" {
+          return Some(Action::SendToMesh {
+            body: "pong!".to_string(),
+            channel: 0.into(),
+            destination: PacketDestination::Node(mesh_packet.from.into()),
+          });
+        }
+      }
+      1 => {
+        // println!("heres the whole packet: {:#?}", &cloned_packet);
+        let decoded_text_message = String::from_utf8(packet_data.payload).unwrap();
+
+        println!("Received text message from channel: {:?}", &decoded_text_message);
+
+        if decoded_text_message == "/ping" {
+          return Some(Action::SendToMesh {
+            body: "pong!".to_string(),
+            channel: 1.into(),
+            destination: PacketDestination::Broadcast,
+          });
+        }
+      }
+      _ => println!("invalid portnum but also shouldnt see this"),
+    },
+
     meshtastic::protobufs::PortNum::WaypointApp => {
       let decoded_waypoint = meshtastic::protobufs::Waypoint::decode(packet_data.payload.as_slice()).unwrap();
 
@@ -179,4 +257,6 @@ fn handle_mesh_packet(mesh_packet: meshtastic::protobufs::MeshPacket) {
       );
     }
   }
+
+  None
 }
