a49a3b1ed031c590800f7284dfedea730a9c465c
Thomas Ranney <thomas@ranney.com>
2025-12-30 20:53:01 -0500

    ok now i just gotta squish these two things together

---
+++ soMuchSignal/src/mysignal.rs
@@ -0,0 +1,358 @@
+use presage::libsignal_service::zkgroup::GroupMasterKeyBytes;
+use presage::model::groups::Group;
+use presage_store_sqlite::SqliteStoreError;
+use tokio;
+// use tokio::runtime::Builder;
+use tokio::sync::mpsc;
+// use tokio::task::LocalSet;
+use tokio::sync::oneshot;
+use tokio::task::spawn_local;
+
+use crate::Profile;
+use crate::ProfileKey;
+use crate::Received;
+use crate::Uuid;
+use crate::logger::Logger;
+use crate::signal::Cmd;
+use crate::signal::attachments_tmp_dir;
+// use crate::signal::get_contacts;
+use crate::signal::list_groups;
+use crate::signal::process_incoming_message;
+use crate::signal::retrieve_profile;
+use crate::signal::run;
+use crate::update::Action;
+
+use futures::StreamExt;
+use futures::pin_mut;
+use tokio::select;
+
+use crate::MyManager;
+use presage::Error;
+use presage::model::contacts::Contact;
+// use presage::store::ContentsStore;
+// pub struct Task<Command, Data> {
+//   cmd: Cmd,
+//   output: oneshot::Sender<Box<T>>,
+// }
+
+type Requester<Data> = mpsc::UnboundedSender<oneshot::Sender<Data>>;
+
+// struct ContactRequest {
+//   output: oneshot::Sender<>
+// }
+
+struct ProfileRequest {
+  output: oneshot::Sender<anyhow::Result<Profile>>,
+  uuid: Uuid,
+  profile_key: Option<ProfileKey>,
+}
+
+pub struct SignalSpawner {
+  send: mpsc::UnboundedSender<Cmd>,
+  contact_requests: Requester<Result<Vec<Contact>, Error<SqliteStoreError>>>,
+  group_requests: Requester<Vec<(GroupMasterKeyBytes, Group)>>,
+  profile_requests: mpsc::UnboundedSender<ProfileRequest>,
+}
+
+impl SignalSpawner {
+  // pub fn new(output: mpsc::UnboundedSender<Action>) -> Self {
+  //   let (send, mut recv) = mpsc::unbounded_channel::<Cmd>();
+  //
+  //   let rt = Builder::new_current_thread().enable_all().build().unwrap();
+  //
+  //   std::thread::spawn(move || {
+  //     let local = LocalSet::new();
+  //
+  //     local.spawn_local(async move {
+  //       let db_path = "/home/mqngo/Coding/rust/signal-tui/plzwork.db3";
+  //
+  //       let config_store = SqliteStore::open_with_passphrase(&db_path, "secret".into(), OnNewIdentity::Trust)
+  //         .await
+  //         .unwrap();
+  //
+  //       while let Some(new_task) = recv.recv().await {
+  //         Logger::log(format!("we gyatt a message but before"));
+  //         let cloned_output = output.clone();
+  //         let cloned_store = config_store.clone();
+  //         _ = run(new_task, cloned_store, cloned_output).await;
+  //       }
+  //       // If the while loop returns, then all the LocalSpawner
+  //       // objects have been dropped.
+  //     });
+  //
+  //     // This will return once all senders are dropped and all
+  //     // spawned tasks have returned.
+  //     Logger::log(format!("blocking on thread ---"));
+  //     rt.block_on(local);
+  //   });
+  //
+  //   Self { send }
+  // }
+
+  pub fn new(mut manager: MyManager, output: mpsc::UnboundedSender<Action>) -> Self {
+    let (send, mut recv) = mpsc::unbounded_channel::<Cmd>();
+
+    // i feel like the compiler should be able to figure out these types
+    let (contacts_sender, mut contact_requests) =
+      mpsc::unbounded_channel::<oneshot::Sender<Result<Vec<Contact>, Error<SqliteStoreError>>>>();
+    let (groups_sender, mut group_requests) =
+      mpsc::unbounded_channel::<oneshot::Sender<Vec<(GroupMasterKeyBytes, Group)>>>();
+
+    let (profile_sender, mut profile_requests) = mpsc::unbounded_channel();
+
+    // let (message_tx, mut message_rx) = mpsc::unbounded_channel();
+
+    spawn_local(async move {
+      // initialize message stream
+      let messages = manager
+        .receive_messages()
+        .await
+        .expect("failed to initialize messages stream");
+      pin_mut!(messages);
+
+      // handle messages in a different "thread" to convert stream to channel
+      // spawn_local(async move {
+      //
+      //   while let Some(message) = messages.next().await {
+      //     _ = message_tx.send(message);
+      //   }
+      // });
+
+      // let max_messages_in_a_row = 1;
+      // let max_commands_in_a_row = 3;
+      let attachments_tmp_dir = attachments_tmp_dir().expect("this is dumb");
+
+      // let mut counter;
+
+      // should enable some gracefull shutdown
+      while !output.is_closed() && !recv.is_closed() {
+        // which we can only wait and see if this was a bad choice
+
+        select! {
+          Some(contacts_output) = contact_requests.recv() => {
+            // let contacts = get_contacts(&manager).await;
+
+            _ = contacts_output.send(Ok(vec![]));
+          }
+
+          Some(groups_output) = group_requests.recv() => {
+            _ = groups_output.send(list_groups(&manager).await);
+          }
+
+          // profile requestss
+          Some(profile_request) = profile_requests.recv() =>
+          {
+            let ProfileRequest {
+            output,
+            uuid,
+            profile_key,
+          } = profile_request;
+            _ = output.send(retrieve_profile(&mut manager, uuid, profile_key).await);
+          }
+
+          Some(content) = messages.next() => {
+            Logger::log("mhhhh some juicy content for you");
+            match &content {
+              Received::QueueEmpty => {
+                _ = output.send(Action::Receive(Received::QueueEmpty));
+                // break;
+              }
+              Received::Contacts => {
+                //println!("got contacts synchronization"),
+              }
+              Received::Content(content) => {
+                // this better be fast lmao
+                process_incoming_message(&mut manager, attachments_tmp_dir.path(), false, &content).await
+              }
+            }
+
+            _ = output.send(Action::Receive(content));
+
+          }
+
+          Some(task) = recv.recv() => {
+            _ = run(&mut manager, task, output.clone()).await;
+            // if counter > max_commands_in_a_row {
+            //   break;
+            // }
+          }
+        }
+      }
+
+      Logger::log("gracefully shutdown ... (hopefully)".to_string());
+
+      // while let Some(new_task) = recv.recv().await {
+      //   // Logger::log(format!("we gyatt a message but before"));
+      //   let cloned_output = output.clone();
+      //
+      //   let error = run(&mut manager, new_task, cloned_output).await;
+      //
+      //   // if let Cmd::Send { .. } = new_task {
+      //   Logger::log(format!("{:?}", error));
+      //   // }
+      // }
+      // If the while loop returns, then all the LocalSpawner
+      // objects have been dropped.
+    });
+
+    // This will return once all senders are dropped and all
+    // spawned tasks have returned.
+
+    Self {
+      send: send,
+      contact_requests: contacts_sender,
+      profile_requests: profile_sender,
+      group_requests: groups_sender,
+    }
+  }
+
+  // #[tokio::main]
+  // pub async fn start(input: output: mpsc::UnboundedSender<Action>) {
+  //   let db_path = "/home/mqngo/Coding/rust/signal-tui/plzwork.db3";
+  //
+  //   let config_store = SqliteStore::open_with_passphrase(&db_path, "secret".into(), OnNewIdentity::Trust)
+  //     .await
+  //     .unwrap();
+  //
+  //   while let Some(new_task) = recv.recv().await {
+  //     Logger::log(format!("we gyatt a message but before"));
+  //     let cloned_output = output.clone();
+  //     let cloned_store = config_store.clone();
+  //     _ = run(new_task, cloned_store, cloned_output).await;
+  //   }
+  //   // If the while loop returns, then all the LocalSpawner
+  //   // objects have been dropped.
+  // }
+
+  pub fn spawn(&self, task: Cmd) {
+    self.send.send(task).expect("Thread with LocalSet has shut down.");
+  }
+
+  pub async fn list_contacts(&self) -> Result<Vec<Contact>, Error<SqliteStoreError>> {
+    let (tx, rx) = oneshot::channel();
+
+    _ = self.contact_requests.send(tx);
+
+    return rx.await.expect("kaboom");
+  }
+
+  pub async fn retrieve_profile(
+    &self,
+    uuid: Uuid,
+    profile_key: Option<ProfileKey>,
+  ) -> anyhow::Result<Profile> {
+    let (tx, rx) = oneshot::channel();
+
+    _ = self.profile_requests.send(ProfileRequest {
+      output: tx,
+      uuid,
+      profile_key,
+    });
+
+    Logger::log("awaiting...");
+    return rx.await.expect("kaboom");
+  }
+
+  pub async fn list_groups(&self) -> Vec<(GroupMasterKeyBytes, Group)> {
+    let (tx, rx) = oneshot::channel();
+
+    _ = self.group_requests.send(tx);
+
+    return rx.await.expect("kaboom once again");
+  }
+
+  pub fn sync_contacts(&self) {
+    _ = self.send.send(Cmd::SyncContacts);
+  }
+}
+
+// fn try_from(content: &Content) -> Result<Thread, UuidError> {
+//   match &content.body {
+//
+//     // [1-1] Message sent by us with another device
+//     ContentBody::SynchronizeMessage(SyncMessage {
+//       sent:
+//         Some(Sent {
+//                   destination_service_id: Some(uuid),
+//                   ..
+//               }),
+//           ..
+//       }) => Ok(Self::Contact(Uuid::parse_str(uuid)?)),
+//
+//       // [Group] message from somebody else
+//       ContentBody::DataMessage(DataMessage {
+//           group_v2:
+//               Some(GroupContextV2 {
+//                   master_key: Some(key),
+//                   ..
+//               }),
+//           ..
+//     })
+//
+//       // [Group] message sent by us with another device
+//       | ContentBody::SynchronizeMessage(SyncMessage {
+//           sent:
+//               Some(Sent {
+//                   message:
+//                       Some(DataMessage {
+//                           group_v2:
+//                               Some(GroupContextV2 {
+//                                   master_key: Some(key),
+//                                   ..
+//                               }),
+//                           ..
+//                       }),
+//                   ..
+//               }),
+//           ..
+//       })
+//       // [Group] message edit sent by us with another device
+//       | ContentBody::SynchronizeMessage(SyncMessage {
+//           sent:
+//               Some(Sent {
+//                   edit_message:
+//                       Some(EditMessage {
+//                           data_message:
+//                               Some(DataMessage {
+//                                   group_v2:
+//                                       Some(GroupContextV2 {
+//                                           master_key: Some(key),
+//                                           ..
+//                                       }),
+//                                   ..
+//                               }),
+//                           ..
+//                       }),
+//                   ..
+//               }),
+//           ..
+//       })
+//       // [Group] Message edit sent by somebody else
+//       | ContentBody::EditMessage(EditMessage {
+//           data_message:
+//               Some(DataMessage {
+//                   group_v2:
+//                       Some(GroupContextV2 {
+//                           master_key: Some(key),
+//                           ..
+//                       }),
+//                   ..
+//               }),
+//           ..
+//       }) => Ok(Self::Group(
+//           key.clone()
+//               .try_into()
+//               .expect("Group master key to have 32 bytes"),
+//       )),
+//       // [1-1] Any other message directly to us
+//       _ => {
+//         let sender = content.metadata.sender.raw_uuid();
+//         let destination = content.metadata.destination.raw_uuid();
+//
+//         if sender != destination {
+//
+//         }
+//
+//         Ok(Thread::Contact())},
+//         }
+// }
---
+++ soMuchSignal/src/update.rs
@@ -0,0 +1,334 @@
+use presage::proto::data_message::{self, Quote};
+use presage::proto::sync_message::Sent;
+use tokio::sync::mpsc::UnboundedSender;
+
+use chrono::TimeDelta;
+
+use futures::{StreamExt, future::FutureExt};
+
+// use presage::model::messages::Received;
+use presage::libsignal_service::content::{Content, ContentBody};
+use presage::libsignal_service::prelude::ProfileKey;
+use presage::proto::receipt_message::Type;
+use presage::proto::{DataMessage, ReceiptMessage, SyncMessage};
+use presage::store::ContentExt;
+use presage::store::Thread;
+
+use std::sync::Arc;
+
+use crate::logger::Logger;
+use crate::*;
+
+#[derive(PartialEq, Debug)]
+pub enum LinkingAction {
+  Url(Url),
+  Success,
+  Fail,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum MessageOption {
+  Reply,
+  React,
+  Edit,
+  Copy,
+  Info,
+  Delete,
+}
+
+// the important one
+#[derive(Debug)]
+pub enum Action {
+  Type(char),
+  Backspace,
+  Send,
+  Nvm,
+
+  Scroll(isize),
+  ScrollGroup(isize),
+  ScrollOptions(isize),
+
+  PickOption,
+  DoOption(MessageOption),
+
+  // Message(Content),
+  Receive(Received),
+  ReceiveBatch(Vec<Content>),
+
+  Link(LinkingAction),
+  Quit,
+}
+
+pub async fn update(model: &mut Model, msg: Action, spawner: &SignalSpawner) -> Option<Action> {
+  match msg {
+    // Action::SetFocus(new_focus) => model.focus = new_focus,
+    Action::Receive(received) => match received {
+      Received::Content(content) => {
+        return handle_message(model, *content);
+      }
+      Received::Contacts => {
+        // update our in memory cache of contacts
+        _ = update_contacts(model, spawner).await;
+      }
+      Received::QueueEmpty => {}
+    },
+
+    Action::ReceiveBatch(received) => {
+      for message in received {
+        handle_message(model, message);
+      }
+    }
+
+    Action::Quit => {
+      // You can handle cleanup and exit here
+      // -- im ok thanks tho
+      model.running_state = RunningState::OhShit;
+    }
+
+    _ => {}
+  }
+
+  None
+}
+
+// pub fn handle_option(
+//   model: &mut Model,
+//   spawner: &SignalSpawner,
+//   option: MessageOption,
+// ) -> Option<Action> {
+//   let chat = model.current_chat();
+//   let message = chat.find_message(chat.message_options.timestamp)?;
+//
+//   // ensure the optino we receive is actually valid for the message
+//   // ie. cant edit / delete someone elses message
+//   if let Metadata::NotMyMessage(_) = message.metadata {
+//     match &option {
+//       &MessageOption::Edit => {
+//         Logger::log(format!("invalid message option: {:?}", option));
+//         return None;
+//       }
+//       &MessageOption::Delete => {
+//         Logger::log(format!("invalid message option: {:?}", option));
+//         return None;
+//       }
+//       _ => {}
+//     }
+//   }
+//
+//   chat.message_options.close();
+//
+//   match option {
+//     MessageOption::Copy => {
+//       let result = execute!(
+//         std::io::stdout(),
+//         CopyToClipboard::to_clipboard_from(
+//           &model.current_chat().selected_message().expect("kaboom").body.body
+//         )
+//       );
+//
+//       if let Err(error) = result {
+//         Logger::log(error)
+//       }
+//
+//       Some(Action::SetMode(Mode::Normal))
+//     }
+//     MessageOption::Reply => {
+//       chat.text_input.mode = TextInputMode::Replying;
+//       Some(Action::SetMode(Mode::Insert))
+//     }
+//     MessageOption::React => {
+//       chat.text_input.mode = TextInputMode::Reacting;
+//       Some(Action::SetMode(Mode::Insert))
+//     }
+//     MessageOption::Edit => {
+//       // kinda gotta find the message twice sometimes cuz "cant have more than one mutable borrow
+//       // yaaaaaaaaaayy..."
+//       let body = chat
+//         .find_message(chat.message_options.timestamp)?
+//         .body
+//         .body
+//         .clone();
+//       chat.text_input.set_content(body);
+//
+//       chat.text_input.mode = TextInputMode::Editing;
+//       Some(Action::SetMode(Mode::Insert))
+//     }
+//     MessageOption::Delete => {
+//       let ts = model.current_chat().message_options.timestamp;
+//       spawner.spawn(Cmd::DeleteMessage {
+//         thread: model.current_chat().thread.clone(),
+//         target_timestamp: ts,
+//       });
+//
+//       model.current_chat().delete_message(ts);
+//
+//       Some(Action::SetMode(Mode::Normal))
+//     }
+//     _ => None,
+//   }
+// }
+
+fn handle_message(model: &mut Model, content: Content) -> Option<Action> {
+  // Logger::log(format!("DataMessage: {:#?}", content.clone()));
+
+  let ts = content.timestamp();
+  let timestamp = DateTime::from_timestamp_millis(ts as i64).expect("this happens too often");
+
+  let Ok(mut thread) = Thread::try_from(&content) else {
+    Logger::log("failed to derive thread from content".to_string());
+    return None;
+  };
+
+  match content.body {
+    ContentBody::DataMessage(DataMessage {
+      body: Some(body),
+      quote,
+      reaction,
+      ..
+    }) => {
+      // Logger::log(format!("DataMessage: {:#?}", body.clone()));
+      // some flex-tape on the thread derivation
+      let mut mine = false;
+      if let Thread::Contact(uuid) = thread {
+        if uuid == model.account.uuid {
+          thread = Thread::Contact(content.metadata.destination.raw_uuid());
+          mine = true;
+        }
+      }
+
+      let quote = if let Some(Quote { id, .. }) = quote {
+        id
+      } else {
+        None
+      };
+
+      let reactions = if let Some(data_message::Reaction {
+        emoji: Some(emoji), ..
+      }) = reaction
+      {
+        Logger::log("it works like this");
+        vec![Reaction {
+          emoji: emoji.chars().nth(0)?,
+          author: content.metadata.sender.raw_uuid(),
+        }]
+      } else {
+        vec![]
+      };
+
+      // insert_message(model, data, thread, ts, mine)
+    }
+
+    ContentBody::SynchronizeMessage(data) => {
+      match data {
+        SyncMessage {
+          sent:
+            Some(Sent {
+              message:
+                Some(DataMessage {
+                  body: Some(body),
+                  quote,
+                  ..
+                }),
+              ..
+            }),
+          // read: read,
+          ..
+        } => {
+          // for receipt in read {
+          //   let Some(aci) = receipt.sender_aci else {
+          //     continue;
+          //   };
+          //   let Some(timestamp) = receipt.timestamp else { continue };
+          //   let Some(aci) = ServiceId::parse_from_service_id_string(&aci) else {
+          //     Logger::log("plz no".to_string());
+          //     return None;
+          //   };
+          //   read_by.push(Receipt {
+          //     sender: aci.raw_uuid(),
+          //     timestamp: DateTime::from_timestamp_millis(timestamp as i64).expect("i think i gotta ditch chrono"),
+          //   });
+          // }
+        }
+        _ => {}
+      }
+    }
+
+    ContentBody::DataMessage(DataMessage {
+      body: None,
+      reaction: Some(reaction),
+      ..
+    }) => {
+      // some flex-tape on the thread derivation
+      if let Thread::Contact(uuid) = thread {
+        if uuid == model.account.uuid {
+          thread = Thread::Contact(content.metadata.destination.raw_uuid());
+        }
+      }
+
+      if let data_message::Reaction {
+        emoji: Some(emoji),
+        target_sent_timestamp: Some(target_ts),
+        ..
+      } = reaction
+      {
+        let reaction = Reaction {
+          emoji: emoji.chars().nth(0)?,
+          author: content.metadata.sender.raw_uuid(),
+        };
+      }
+
+      // insert_message(model, data, thread, ts, mine)
+    }
+    _ => {}
+  }
+
+  None
+}
+
+pub async fn update_contacts(model: &mut Model, spawner: &SignalSpawner) -> anyhow::Result<()> {
+  Logger::log("updating contacts".to_string());
+  for contact in spawner.list_contacts().await? {
+    // Logger::log(format!("{}", contact.inbox_position));
+    if model.contacts.contains_key(&contact.uuid) {
+      Logger::log("already_gyatt key".to_string());
+      continue;
+    } else {
+      let profile_key = match contact.profile_key.clone().try_into() {
+        Ok(bytes) => Some(ProfileKey::create(bytes)),
+        Err(_) => {
+          // Logger::log(format!("died on this dude: {:#?}", contact));
+          None
+        }
+      };
+
+      let profile = match spawner.retrieve_profile(contact.uuid, profile_key).await {
+        Ok(x) => x,
+        Err(_) => continue,
+      };
+
+      let Some(contacts) = Arc::get_mut(&mut model.contacts) else {
+        Logger::log("didnt get off so easy".to_string());
+        return Ok(());
+      };
+
+      contacts.insert(contact.uuid, profile.clone());
+    }
+  }
+  Ok(())
+}
+
+impl Model {
+  pub async fn update_groups(self: &mut Self, spawner: &SignalSpawner) -> anyhow::Result<()> {
+    Logger::log("updating groups".to_string());
+    for (key, group) in spawner.list_groups().await {
+      if !self.groups.contains_key(&key) {}
+      let Some(groups) = Arc::get_mut(&mut self.groups) else {
+        Logger::log("didnt get off so easy".to_string());
+        continue;
+      };
+
+      groups.insert(key, group);
+    }
+    Ok(())
+  }
+}
